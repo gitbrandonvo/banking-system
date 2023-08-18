7d7a54e0c1a0da0dd8fb4a40074fee7cdbb7df7f
Brandon Vo <code@brandonvo.dev>
2023-08-17 22:39:58 -0600

    initial commit with completed readme & code

---
+++ banking-system.js
@@ -0,0 +1,164 @@
+class Bank {
+    accountsList = [];
+    constructor(accounts = []) {
+        this.accountsList = accounts;
+    }
+
+    //Display accounts
+    displayAccounts = () => {
+        console.log("Established accounts:")
+        for (const account of this.accountsList) {
+            console.log(`--------------------------\n`)
+            for (const property in account) {
+                console.log(`${property}: ${account[property]}`);
+            }
+            console.log(`\n`)
+        }
+    }
+
+    //Add an account to accountsList array in bank obj
+    addAccount = (Account) => {
+        this.accountsList.push(Account);
+    }
+
+    //Verify account number is a string, exists within accountsList and amount is a number
+    //Returns index of where the account is located in the accountList array, otherwise -1.
+    isAccountAndAmountIsNumber = (acctNumber, amount) => {
+        if (typeof acctNumber == "string" && typeof amount == "number") {
+            for (let i = 0; i < this.accountsList.length; i++) {
+                const account = this.accountsList[i];
+                if (account.accountNumber == acctNumber) {
+                    return i;
+                }
+            } return -1;
+        } else {
+            console.log(`Invalid account number or amount.`);
+            return -1;
+        }
+    }
+
+    /*Deposit function
+    Validates if isAccountAndAmountIsNumber() != -1 and uses index returned by the function.
+    Returns boolean*/
+    deposit = (acctNumber, depositAmount) => {
+        const accountIndex = this.isAccountAndAmountIsNumber(acctNumber, depositAmount);
+        if (accountIndex != -1) {
+            const account = this.accountsList[accountIndex];
+            account.balance += depositAmount
+            console.log(`Deposited $${depositAmount}. Balance: $${account.balance}`)
+            return true;
+        } return false;
+    }
+
+    /*Withdraw function
+    - Validates if isAccountAndAmountIsNumber() != -1 and uses index returned by the function.
+    - If account exists, it checks the account type and withdraws only if it is a Checkings or Savings account. 
+    - If it is a checkings account, the overdraft limit is added to the balance to evaluate if a withdraw is allowed. Assumption: bank allows withdrawing existing balance + overdraft limit*/
+    withdraw = (acctNumber, withdrawAmount) => {
+        const accountIndex = this.isAccountAndAmountIsNumber(acctNumber, withdrawAmount);
+        if (accountIndex != -1) {
+            const account = this.accountsList[accountIndex];
+            if (account.hasOwnProperty("accountType")) {
+                switch (account.accountType) {
+                    case "Checking Account":
+                        if ((account.balance + account.overdraftLimit) >= withdrawAmount) {
+                            account.balance -= withdrawAmount;
+                            console.log(`Amount withdrawn: ${withdrawAmount}. Balance remaining: $${account.balance}`)
+                            return true;
+                        }
+                        console.log(`Withdraw for $${withdrawAmount} unsuccessful due to insufficient balance.`);
+                        return false;
+
+                    case "Savings Account":
+                        if (account.balance >= withdrawAmount) {
+                            account.balance -= withdrawAmount;
+                            console.log(`Amount withdrawn: $${withdrawAmount}. Balance remaining: $${account.balance}`)
+                            return true;
+                        }
+                        console.log(`Withdraw for $${withdrawAmount} unsuccessful due to insufficient balance.`);
+                        return false;
+
+                    default:
+                        console.log(`Invalid account type.`);
+                        return false;
+                }
+
+            }
+        }
+    }
+
+    //Calculate interest
+    //Given an account number and whole number interest rate, this function will return the interest amount.
+    calculateInterest = (acctNumber, wholeInterestRate) => {
+        const savingsAccountIndex = this.isAccountAndAmountIsNumber(acctNumber, wholeInterestRate);
+        if (savingsAccountIndex != -1) {
+            const account = this.accountsList[savingsAccountIndex];
+            return (account.balance * (wholeInterestRate / 100));
+        }
+    }
+
+    //Transfer
+    //Validates if isAccountAndAmountIsNumber() != -1 and uses index returned by the function.
+    //If withdraw is successful, a deposit is made to the other account.
+    transfer = (fromAccountNumber, toAccountNumber, transferAmount) => {
+        const fromAccountIndex = this.isAccountAndAmountIsNumber(fromAccountNumber, transferAmount);
+        const toAccountIndex = this.isAccountAndAmountIsNumber(toAccountNumber, transferAmount)
+
+        if ((fromAccountIndex != -1) && (toAccountIndex != -1)) {
+            const fromAccount = this.accountsList[fromAccountIndex];
+            const toAccount = this.accountsList[toAccountIndex];
+            const withdrawn = this.withdraw(fromAccount.accountNumber, transferAmount);
+            if (withdrawn) {
+                this.deposit(toAccount.accountNumber, transferAmount);
+                console.log(`Transferred: $${transferAmount}. Remaining balance: ${fromAccount.balance}`);
+                return true;
+            } else {
+                console.log(`Transfer of $${transferAmount} unsuccessful.`);
+                return false;
+            }
+        }
+    }
+}
+
+//Classes
+
+class Account {
+    constructor(accountHolder, accountNumber, balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
+
+class CheckingAccount extends Account {
+
+    constructor(accountHolder, accountNumber, balance, overdraftLimit) {
+        super(accountHolder, accountNumber, balance);
+        this.accountType = "Checking Account";
+        this.overdraftLimit = overdraftLimit;
+    }
+
+}
+class SavingsAccount extends Account {
+
+    constructor(accountHolder, accountNumber, balance) {
+        super(accountHolder, accountNumber, balance);
+        this.accountType = "Savings Account";
+    }
+
+}
+
+//Client code:
+
+const bank = new Bank();
+bank.addAccount(new CheckingAccount("Brandon Vo", "A001", 1000, 300));
+bank.addAccount(new CheckingAccount("Aliah Hope", "A002", 1500, 300));
+bank.addAccount(new SavingsAccount("Jered Smith", "A003", 3000));
+console.log(bank.accountsList[0].accountType);
+bank.withdraw("A001", 200);
+bank.withdraw("A001", 10000); //unsuccessful
+bank.deposit("A001", 300);
+bank.transfer("A002", "A003", 5000); //unsuccessful
+bank.transfer("A002", "A003", 500);
+bank.withdraw("A003", bank.calculateInterest("A003", 5)); //withdraw based on given account number & interest rate 
+bank.displayAccounts();
---
+++ readme.md
@@ -0,0 +1,107 @@
+# Banking System Exercise
+
+Welcome! This was practice creating a Banking System in Javascript! This project simulates a basic banking system that handles different types of accounts, transactions, and interest calculations.
+
+### Requirements
+Create a banking system that handles different types of accounts, transactions, and interest calculations. Implement the following features:
+   - Create a base class `Account` with properties: `accountNumber`, `accountHolder`, and `balance`.
+   - Create subclasses `CheckingAccount` and `SavingsAccount` that inherit from `Account`.
+   - `CheckingAccount` should have an additional property `overdraftLimit`.
+   - Implement methods for depositing and withdrawing money from accounts. Consider overdraft limits for `CheckingAccount`.
+   - Implement a method to calculate the interest for an account with a given interest rate.
+   - Create a class `Bank` that maintains a collection of accounts.
+   - Implement methods to add accounts to the bank, perform transactions, and display account details.
+   - Client code demonstrating use.
+   - Challenges & Considerations:
+   - Handling cases like insufficient balance and invalid account numbers
+   - Potential enhancements like fund transfers between accounts.
+
+### Implemented Overview
+
+### Account Classes
+
+- The `Account` class serves as the base class, featuring properties like `accountNumber`, `accountHolder`, and `balance`.
+- Derived classes include `CheckingAccount` and `SavingsAccount`, each inheriting from `Account`.
+- `CheckingAccount` introduces an additional property, `overdraftLimit`.
+
+### Operations
+
+- Methods have been implemented for depositing and withdrawing money from accounts. Special consideration is given to the overdraft limit for `CheckingAccount`.
+- A method calculates interest for an account based on a given interest rate.
+
+### Bank Class
+
+- The `Bank` class maintains a collection of accounts.
+- It provides methods to add accounts, perform transactions, and display account details.
+
+### Client Code
+
+- The client code demos on creation of account instances and executes transactions using the `Bank` class.
+- After transactions, transaction/error message and balances are displayed.
+
+## Challenges and Considerations
+
+- Handles cases like insufficient balance and invalid account numbers
+- Fund transfers between accounts
+
+## Usage Methods
+
+1. Open file directly in Github, copy contents, and paste into developer tools console (Shortcut: F12 on most browsers)
+2. Clone this repository, open in a supported development environment, and run the code.
+
+### Basic Usage
+
+```javascript
+// Create instances of different types of accounts
+const bank = new Bank();
+const checkingAccount = new CheckingAccount("Brandon Vo", "A001", 1000, 300));
+const savingsAccount = new SavingsAccount("Jered Smith", "A003", 3000);
+
+// Add accounts to the bank
+bank.addAccount(checkingAccount);
+bank.addAccount(savingsAccount);
+
+// Perform transactions
+bank.deposit("A001", 300);
+bank.withdraw("A001", 200);
+bank.transfer("A002", "A003", 500);
+// Display account details
+bank.displayAccounts();
+```
+
+### Client Code
+
+```javascript
+// Create a bank instance
+const bank = new Bank();
+
+// Create account instances
+const checkingAccount = new CheckingAccount("Brandon Vo", "A001", 1000, 300));
+const checkingAccount2 = new CheckingAccount("Aliah Hope", "A002", 1500, 300));
+const savingsAccount = new SavingsAccount("Jered Smith", "A003", 3000);
+
+// Add accounts to the bank
+bank.addAccount(checkingAccount);
+bank.addAccount(savingsAccount);
+
+// Perform transactions using the bank
+bank.deposit("A001", 300);
+bank.withdraw("A001", 200);
+bank.transfer("A002", "A003", 500);
+
+// Display account details after transactions
+bank.displayAccounts();
+```
+
+## My Reflections
+
+The banking system meets requirements and works as expected. While requirements were the focus, post review, I see that there are definite improvements can be made.
+- This project mainly focused on backend logic.. a natural progression could involve creating a user interface to interact with the system.
+- Refining conditional logic: refining logic and adding validation for acceptable deposits, withdraws, overdraft limits, transfers, edge cases, etc.
+- Data persistence could be implemented through something like a database or file system. 
+- Address data integrity: can be addressed using a RDMS (ex. transactions).
+- With RDMS: Prevent duplicate account numbers and unique account holders
+- Utilizing more robust error handling starting with methods like try-catch(-finally) blocks
+- Encapsulate fields (#)
+- Implement account history?? A possible thought on implementation: add another account property assigned to an array that will store future transactions. Create a class representing a transaction. Implement into the existing methods with details of each transaction and adding transaction objects to that array
+- Using constants for the account types
